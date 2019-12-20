7a26297b81c3574f49812825cf77901d65adb31e
starrpatty28 <starrpatty28@gmail.com>
2019-12-20 15:22:17 -0800

    Had to get the gorilla mux package from Github in order to run on the router instead of the http server

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
 // 4. Define the Structure of my Articles
 type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
@@ -19,28 +21,30 @@ type Articles []Article
 
 // 6. All Articles Function
 func allArticles(w http.ResponseWriter, r *http.Request) {
-	
+
 	// Dummy Data
-	articles := Articles {
+	articles := Articles{
 		Article{Title: "Golang", Desc: "Google Golang Language", Content: "Hello World GoLang #GangGang"},
 	}
-		fmt.Println("Endpoint Hit: All Articles Endpoint")
-		json.NewEncoder(w).Encode(articles)
-} 
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 // 1. Homepage Function
-func homePage (w http.ResponseWriter, r *http.Request)  {
-		fmt.Fprintf(w, "Homepage Endpoint Hit")	
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
 // 2. Handle Request Function
 func handleRequest() {
-		http.HandleFunc("/", homePage)
-		http.HandleFunc("/articles", allArticles)
-		log.Fatal(http.ListenAndServe(":8081", nil))
-} 
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
 
 // 3. Main Function
-func main () {
+func main() {
 	handleRequest()
 }
